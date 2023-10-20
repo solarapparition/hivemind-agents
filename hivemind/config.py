ae99b63099a3fb195ea2bfd8eb6d82fd78f979f2
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2023-10-20 09:56:53 -0400

    update project config

--- hivemind/config.py
+++ hivemind/config.py
@@ -4,6 +4,7 @@ from os import makedirs
 
 from hivemind.toolkit.yaml_tools import yaml
 
+DATA_DIR = Path(".data")
 BASE_WORK_DIR = Path(".data/shared_workspace")
 makedirs(BASE_WORK_DIR, exist_ok=True)
 EMBEDCHAIN_DATA_DIR = Path(".data/embedchain")
@@ -12,6 +13,9 @@ SECRETS_LOCATION = Path(".data/secrets.yaml")
 makedirs(SECRETS_LOCATION.parent, exist_ok=True)
 LANGCHAIN_CACHE_DIR = Path(".data/cache")
 makedirs(LANGCHAIN_CACHE_DIR, exist_ok=True)
+TEST_DIR = DATA_DIR / "test"
+makedirs(TEST_DIR, exist_ok=True)
+BROWSERPILOT_DATA_DIR = DATA_DIR / "browserpilot"
 secrets: dict[str, str] = yaml.load(SECRETS_LOCATION)
 BROWSERLESS_API_KEY = secrets["BROWSERLESS_API_KEY"]
 SERPER_API_KEY = secrets["SERPER_API_KEY"]
