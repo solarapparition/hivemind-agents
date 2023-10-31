b52519f3a908b2e40654fcff9fd2e0880d28754e
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2023-10-31 09:42:21 -0400

    factor out text validation

--- hivemind/daemons/browser.py
+++ hivemind/daemons/browser.py
@@ -20,12 +20,13 @@ from hivemind.config import (
     BROWSERPILOT_DATA_DIR,
     CHROMEDRIVER_LOCATION,
 )
-from hivemind.toolkit.models import query_model, exact_model
+from hivemind.toolkit.models import query_model, precise_model
 from hivemind.toolkit.text_formatting import dedent_and_strip, extract_blocks
 from hivemind.toolkit.autogen_support import get_last_user_reply
 from hivemind.toolkit.browserpilot_support import run_with_instructions
 from hivemind.toolkit.semantic_filtering import filter_semantic_html
 from hivemind.toolkit.webpage_inspector import WebpageInspector
+from hivemind.toolkit.text_validation import validate_text, find_llm_validation_error
 
 langchain.llm_cache = SQLiteCache(
     database_path=str(LANGCHAIN_CACHE_DIR / ".langchain.db")
@@ -74,81 +75,6 @@ def make_hivemind_user_proxy(
         system_message="Reply TERMINATE if the task has been solved or cannot be solved by the assistant.",
         function_map=function_map,
     )
-
-
-def find_llm_validation_error(validation_messages: Sequence[BaseMessage]) -> str | None:
-    """Run validation using an LLM. A specific output format is expected:
-    If the message has no errors:
-    ```text
-    N/A
-    ```
-
-    If the message has errors:
-    ```text
-    <error>
-    ```
-    Where <error> contains the string "Error:".
-    """
-    result = query_model(exact_model, validation_messages, printout=False)
-    error = extract_blocks(result, "text")
-    error_flag = "ERROR:"
-    if not error or ("N/A" not in error[-1] and error_flag not in error[-1]):
-        raise ValueError(f"Unable to extract error validation result:\n{error}")
-    error_text = error[-1].strip()
-    return error_text if error_flag in error_text else None
-
-
-def validate_text(
-    text: str, requirements: str, context: str | None = None
-) -> tuple[bool, str]:
-    """Validate text output based on freeform requirements. Requirements should be relatively simple—something that a human who understands the context could check in a few seconds."""
-    instructions = """
-    # MISSION
-    You are a text validation bot. Your purpose is to validate that the text that you're given meets certain requirements.
-    
-    # VALIDATION CONTEXT
-    Here is some context in which the text is being evaluated to help with validation:
-    ```text
-    {context}
-    ```
-
-    # TEXT REQUIREMENTS
-    Here are the requirements to check for the text you're given:
-    ```text
-    {requirements}
-    ```
-
-    # INPUT
-    The text you're given is:
-    ```text
-    {text}
-    ```
-    
-    # OUTPUT
-    Check whether the text meets the requirements.
-
-    If the text meets the requirements, output the following (include the backtick delimiters):
-    ```text
-    N/A
-    ```
-
-    If the text does not meet the requirements, output the following (fill in the error message):
-    ```text
-    ERROR: {{error}}
-    ```
-    You may output other comments, but all other comments must be outside the ```text``` block.
-    """
-    instructions = dedent_and_strip(instructions).format(
-        text=text, requirements=requirements, context=context
-    )
-    error = find_llm_validation_error([SystemMessage(content=instructions)])
-    error_message = dedent_and_strip(
-        """
-        {error}
-        Original Text: "{text}"
-        """
-    ).format(error=error, text=text)
-    return error is None, "" if error is None else error_message
 
 
 def test_validate_text() -> None:
