e27b5fc0fb484ab391c875db375c53360a22262f
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2023-11-04 12:32:06 -0400

    add handling for failed browserpilot commands

--- hivemind/toolkit/browserpilot_support.py
+++ hivemind/toolkit/browserpilot_support.py
@@ -2,7 +2,16 @@
 
 from browserpilot.agents.gpt_selenium_agent import GPTSeleniumAgent
 
-def run_with_instructions(agent: GPTSeleniumAgent, instructions: str) -> None:
+
+def run_browserpilot_with_instructions(
+    agent: GPTSeleniumAgent, instructions: str, handle_error: bool = True
+) -> str | None:
     """Run browserpilot agent with instructions."""
     agent.set_instructions(instructions)
-    agent.run()
+    try:
+        agent.run()
+    except Exception as error:
+        if not handle_error or "Failed to execute" not in str(error):
+            raise error
+        return str(error)
+    return None
