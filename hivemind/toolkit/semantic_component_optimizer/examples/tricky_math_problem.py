4be5a14089a4496efc7121962a727ac63577bbc7
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2023-11-09 08:38:38 -0500

    rename

---
+++ hivemind/toolkit/semantic_component_optimizer/examples/tricky_math_problem.py
@@ -0,0 +1,14 @@
+
+from hivemind.toolkit.semantic_component_optimizer
+# problem:
+"""
+Triangle p has sides of length x, y and z. If x = 5 and y = 8, what is the perimeter of triangle p?
+(1) Triangle p is isosceles.
+(2) z is a prime number less than 7.
+
+A - (1) ALONE is sufficient, but (2) alone is not sufficient.
+B - (2) ALONE is sufficient, but (1) alone is not sufficient.
+C - TOGETHER are sufficient, but NEITHER ALONE is sufficient.
+D - EACH ALONE is sufficient.
+E - NEITHER ALONE NOR TOGETHER is the statements sufficient.
+"""
