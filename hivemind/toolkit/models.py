0771b0190dca2760d874c5e3e76a71272fc05749
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2023-12-08 09:37:41 -0500

    update query to pause for debugging

--- hivemind/toolkit/models.py
+++ hivemind/toolkit/models.py
@@ -7,7 +7,7 @@ from langchain.schema import BaseMessage
 from langchain.chat_models.base import BaseChatModel
 from langchain.chat_models import ChatOpenAI, ChatAnthropic
 
-precise_model = ChatOpenAI(temperature=0, model_name="gpt-4", verbose=False)  # type: ignore
+precise_model = ChatOpenAI(temperature=0, model_name="gpt-4-1106-preview", verbose=False)  # type: ignore
 creative_model = ChatOpenAI(temperature=0.7, model_name="gpt-4", verbose=False)  # type: ignore
 # super_creative_model = ChatOpenAI(temperature=1.0, model_name="gpt-4", verbose=False)  # type: ignore
 super_creative_model = ChatOpenAI(temperature=1.0, model_name="gpt-4-1106-preview", verbose=False)  # type: ignore
@@ -20,15 +20,14 @@ def query_model(
     model: BaseChatModel,
     messages: Sequence[BaseMessage],
     color: str = Fore.RESET,
-    # color: int = 0,
     preamble: str | None = None,
     printout: bool = True,
 ) -> str:
     """Query an LLM chat model. `preamble` is printed before the result."""
+    breakpoint()
     if preamble is not None and printout:
         print(f"\033[1;34m{preamble}\033[0m")
     result = model(list(messages)).content
     if printout:
         print(f"{color}{result}{Fore.RESET}")
-        # print(f"\033[{color}m{result}\033[0m")
     return result
